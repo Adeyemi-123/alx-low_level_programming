1581ce150ec812fa09960bc42c5dc1750d40f18e
Anthony <you@example.com>
2023-06-21 00:26:59 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - absolute numbers
+ * @k: the integer we are working on
+ * Return: return the absolute values of k and -k
+ */
+
+int _abs(int k)
+{
+	if (k > 0)
+		return (k);
+	else
+		return (-k);
+}
