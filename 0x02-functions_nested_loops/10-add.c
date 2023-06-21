32718ba2549ead6f2662dc6c1e20cfa9cc551dc5
Anthony <you@example.com>
2023-06-21 16:46:50 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Description: ' adds two integers 'a' and 'b' and returns the result.'
+ * Return: return results for 'a' and 'b'
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * times_table - 9 times table
+ * Description: prints the 9 times table, starting with 0
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (
+}
