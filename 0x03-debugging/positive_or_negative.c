3ba29a6b51280fbb2993e6181b9a01f6f1c687b3
Anthony <you@example.com>
2023-06-22 02:00:48 -0700

    Task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Debugging codes
+ * Description: Testing, debugging
+ * @i: working with i
+ * Return: void
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+
+
+}
