70154052441a9dfe68468c4377b465301f67bdbc
Anthony <you@example.com>
2023-07-07 09:19:06 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: the number of argument
+ * @argv: void
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
