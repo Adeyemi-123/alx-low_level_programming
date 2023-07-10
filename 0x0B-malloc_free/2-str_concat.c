9880c2b23e1a5ed5d5c7567f7a2235a2cf7d1d3b
Anthony <you@example.com>
2023-07-10 16:07:01 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -38,6 +38,10 @@ char *str_concat(char *s1, char *s2)
 	strcpy(result, s1);
 	strcat(result, s2);
 
+	if (s1 == NULL || s2 == NULL || (s1 == NULL && s2 == NULL))
+	{
+		return (result);
+	}
 	return (result);
 
 }
