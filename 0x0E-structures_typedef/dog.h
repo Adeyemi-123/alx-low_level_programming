06479592cf3a0f6f021e2d6bfa4a9083a228a7e8
Anthony <you@example.com>
2023-07-17 07:05:26 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+ * struct dog: a C structure
+ *
+ * @name: a pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: A pointer to the owner of the dog
+ * Return: void
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
