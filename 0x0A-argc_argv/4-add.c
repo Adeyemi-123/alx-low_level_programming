051ab60998e7ecab3a6a7048579c1bc6e2920622
Adeyemi-123 <Kaluokoro608@gmail.co>
2023-08-04 21:59:20 -0700

    fine

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * @argc: size of array pointer
+ * @argv: pointer of array
+ * Return: return 1 or sum
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+	int num;
+
+	sum = 0;
+
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+
+		if (!isdigit(num))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+	return (sum);
+}
