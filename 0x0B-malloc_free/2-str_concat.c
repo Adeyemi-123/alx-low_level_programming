a83536e012e7fe8615606eef63688837b17a83ac
Adeyemi-123 <Kaluokoro608@gmail.co>
2023-08-08 14:03:18 -0700

    All should be fine now

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,6 +20,15 @@ char *str_concat(char *s1, char *s2)
 	len2 = strlen(s2);
 	len = len1 + len2;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	str = (char *)malloc((len + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
