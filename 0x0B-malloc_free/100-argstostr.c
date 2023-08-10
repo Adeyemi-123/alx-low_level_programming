f1623efe0a2b34da6df46cb266a36471b6a7cb75
Adeyemi-123 <Kaluokoro608@gmail.co>
2023-08-09 19:05:23 -0700

    It just worked

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - function that concatenates all the arguments
+ * @ac: size of pointer av
+ * @av: pointer that contains all the argumenr
+ * Return: returns pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i;
+	int len;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		len += strlen(av[i]);
+	}
+
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		strcat(str, av[i]);
+		strcat(str, "\n");
+	}
+	return (str);
+
+	free(str);
+}
