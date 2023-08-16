e08e7c17d43983b2d5d9e07ff9f93102ed5dd42d
Adeyemi-123 <Kaluokoro608@gmail.co>
2023-08-15 17:54:06 -0700

    interesting journey learning alot

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - new dog type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: return new_dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	new_dog->name = strdup(name);
+	new_dog->age = age;
+	new_dog->owner = strdup(owner);
+
+	return (new_dog);
+}
