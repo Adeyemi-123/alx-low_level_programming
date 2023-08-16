4128c67b158fe45a47de2ded7ddd14331c244037
Adeyemi-123 <Kaluokoro608@gmail.co>
2023-08-15 18:14:29 -0700

    Easier than I thought

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 
 /**
+ * free_dog - function that frees struct dog
+ * @d: pointer to members of struct
  */
 
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
 
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
