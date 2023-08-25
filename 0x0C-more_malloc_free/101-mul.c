4c5759fd863fb3437362f6f4e6f8d1de8a76a316
ADeyemi-123 <Kaluokoro608@gmail.com>
2023-08-25 11:19:34 +0100

    God help me

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include "main.h"
 
+
 char *multiplyStrings(char *num1, char *num2)
 {
 	int len1 = strlen(num1);
@@ -38,43 +39,5 @@ int isNumeric(const char *str)
 		str++;
 	}
 	return (1);
-}
-
-int main(int argc, char *argv[])
-{
-	char *num1;
-	char *num2, *result;
-
-	if (argc != 3 || !isNumeric(argv[1]) || !isNumeric(argv[2])) 
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	num1 = argv[1];
-	num2 = argv[2];
-
-	while (*num1 == '0' && *(num1 + 1) != '\0')
-	{
-		num1++;
-	}
-	while (*num2 == '0' && *(num2 + 1) != '\0')
-	{
-		num2++;
-	}
-
-	if (*num1 == '\0' || *num2 == '\0')
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	result = multiplyStrings(num1, num2);
-
-	printf("%s\n", result);
-
-	free(result);
-
-	return (0);
 }
 
