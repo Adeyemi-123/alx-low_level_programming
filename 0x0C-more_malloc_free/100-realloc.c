380ed175e915d749329d6d80b9d8c4cb2fc35bbd
ADeyemi-123 <Kaluokoro608@gmail.com>
2023-08-25 10:51:23 +0100

    First time using git bash

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_realloc - function that reallocates a memory block using malloc and free
+ * @ptr: a pointer in the function
+ * @ old_size: oldsize in the function
+ * @new_size: new size in the function
+ * Return: return newly allocated space
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (ptr == NULL) 
+	{
+		return (malloc(new_size));
+	}
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+
+	free(ptr);
+
+	return (new_ptr);
+}
