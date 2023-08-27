8543ad86b1b24918b92c86ba416136ff0b305bcc
ADeyemi-123 <Kaluokoro608@gmail.com>
2023-08-27 05:27:20 +0100

    okay now:

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -41,3 +41,40 @@ int isNumeric(const char *str)
 	return (1);
 }
 
+int main(int argc, char *argv[])
+{
+        char *num1;
+        char *num2, *result;
+
+        if (argc != 3 || !isNumeric(argv[1]) || !isNumeric(argv[2]))
+        {
+                printf("Error\n");
+                return (98);
+        }
+
+        num1 = argv[1];
+        num2 = argv[2];
+
+        while (*num1 == '0' && *(num1 + 1) != '\0')
+        {
+                num1++;
+        }
+        while (*num2 == '0' && *(num2 + 1) != '\0')
+        {
+                num2++;
+        }
+
+        if (*num1 == '\0' || *num2 == '\0')
+        {
+                printf("0\n");
+                return (0);
+        }
+
+        result = multiplyStrings(num1, num2);
+
+        printf("%s\n", result);
+
+        free(result);
+
+	return (0);
+}
