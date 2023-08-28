5f1d12fcc33b8d87faf14cb946b38f508aeb6f9d
ADeyemi-123 <Kaluokoro608@gmail.com>
2023-08-29 00:38:23 +0100

    God has done it

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: size of argv
+ * @argv: pointer of arrays
+ * Return: Always (0) success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	unsigned char *main_ptr = (unsigned char *)main;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_ptr[i]);
+
+		if (i != num_bytes - 1)
+		{
+			printf(" ");
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
