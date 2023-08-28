b6e8753899037c0eaf88c3de871a18876ca17c7d
ADeyemi-123 <Kaluokoro608@gmail.com>
2023-08-28 23:07:18 +0100

    Trying a second time

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,6 +3,60 @@
 #include <stdio.h>
 
 /**
+ * print_name_as_is - function that prints a name as it is
+ * @name: name to be printed
+ */
+
+void print_name_as_is(char *name)
+{
+	char *prefix = "Hello, my name is ";
+	int i = 0;
+
+	while (prefix[i])
+	{
+		write(1, &prefix[i], 1);
+		i++;
+	}
+
+	i = 0;
+	while (name[i])
+	{
+		write(1, &name[i], 1);
+		i++;
+	}
+
+	write(1, "\n", 1);
+}
+
+/**
+ * print_name_uppercase - prints name in uppercase
+ * @name: name to be printed
+ */
+
+void print_name_uppercase(char *name)
+{
+	char *prefix = "Hello, my uppercase name is ";
+	int i = 0;
+
+	while (prefix[i])
+	{
+		write(1, &prefix[i], 1);
+		i++;
+	}
+
+	i = 0;
+	while (name[i])
+	{
+		char uppercase_char = name[i];
+		if (uppercase_char >= 'a' && uppercase_char <= 'z')
+		{
+			uppercase_char += 'A' - 'a';
+		}
+		write(1, &uppercase_char, 1);
+		i++;
+	}
+}
+/**
  * print_name - function that prints a name.
  * @name: name to be printed
  * @f: pointer to retrieve name
