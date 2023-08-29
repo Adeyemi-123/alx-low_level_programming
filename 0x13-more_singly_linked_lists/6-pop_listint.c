b32b9abb07ebdb0d12597dec3781d2b39cce8cfd
ADeyemi-123 <Kaluokoro608@gmail.com>
2023-08-29 23:49:30 +0100

    God has done it

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,11 +10,13 @@
 
 int pop_listint(listint_t **head)
 {
+	listint_t *temp = *head;
+	int data = temp->n;
+
+
 	if (head == NULL || *head == NULL)
 		return (0);
 
-	listint_t *temp = *head;
-	int data = temp->n;
 
 	*head = (*head)->next;
 	free(temp);
