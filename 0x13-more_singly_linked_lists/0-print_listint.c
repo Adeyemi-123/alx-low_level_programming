895083b8e2c2d9741fc1036e2f4dfef6450e46ac
ADeyemi-123 <Kaluokoro608@gmail.com>
2023-08-29 02:38:08 +0100

    God has done it

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <lists.h>
+
+/**
+ * print_listint - function that rints all the elements
+ * of a listint_t list
+ * @h: pointer to the nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t size = 0;
+
+	while (h->str != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s", h->len, h->str);
+
+		h = h->next;
+		size++;
+	}
+	
+	return (size);
+}
