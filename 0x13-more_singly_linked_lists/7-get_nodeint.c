3aa10a47ad406c60b07ffb19c474acec5f7f1cdd
ADeyemi-123 <Kaluokoro608@gmail.com>
2023-08-30 01:59:28 +0100

    God has done it

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,9 +10,10 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current = head;
+	listint_t *current;
 	unsigned int count = 0;
 
+	current = head;
 	while (current != NULL && count <= 0)
 	{
 		if (count == index)
