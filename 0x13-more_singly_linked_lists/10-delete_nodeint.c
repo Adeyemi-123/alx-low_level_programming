2c40ad131b52152c4431f9403c3151a8acdbb0b8
ADeyemi-123 <Kaluokoro608@gmail.com>
2023-08-30 03:27:01 +0100

    God has done it

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *current;
+	unsigned int count;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	current = *head;
+	count = 0;
+
+	while (current != NULL && count < index - 1)
+	{
+		current = current->next;
+		count++;
+	}
+
+	if (current == NULL || current->next == NULL)
+		return (-1);
+
+	temp = current->next;
+	current->next = temp->next;
+	free(temp);
+
+	return (1);
+}
